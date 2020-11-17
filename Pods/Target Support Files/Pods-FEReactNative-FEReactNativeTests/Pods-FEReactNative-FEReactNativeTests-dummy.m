36a887d7ffbe8b3b1f6d1eb4ddd108ace13bb7f2
Ericson Cepeda <ericson@twnel.com>
2020-11-17 20:21:31 +0100

    FIRST COMMIT gitignore Pods deleted

--- Pods/Target Support Files/Pods-FEReactNative-FEReactNativeTests/Pods-FEReactNative-FEReactNativeTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FEReactNative_FEReactNativeTests : NSObject
-@end
-@implementation PodsDummy_Pods_FEReactNative_FEReactNativeTests
-@end
--- Pods/Target Support Files/Pods-FEReactNative-tvOSTests/Pods-FEReactNative-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FEReactNative_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_FEReactNative_tvOSTests
-@end
